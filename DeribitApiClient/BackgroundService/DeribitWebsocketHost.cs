d3c3ff6ae16d279099bfe1394b58f8ea35606815
gyurebalint <gyurebalint@gmail.com>
2023-07-12 18:18:37 +0200

    generic host added, loads configuration

---
+++ DeribitApiClient/BackgroundService/DeribitWebsocketHost.cs
@@ -0,0 +1,22 @@
+﻿
+
+using Microsoft.Extensions.Hosting;
+
+namespace DeribitApiClient.WebSocketService
+{
+    internal class DeribitWebsocketHost : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Service started...");
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Service shut down...");
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- DeribitApiClient/Program.cs
+++ DeribitApiClient/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace DeribitApiClient
+﻿using DeribitApiClient.WebSocketService;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DeribitApiClient
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+            builder.Services.AddHostedService<DeribitWebsocketHost>();
+
+            IHost host = builder.Build();
+            host.Run();
         }
     }
 }
